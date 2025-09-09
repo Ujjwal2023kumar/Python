e491ca3ac3177e36eb465ca7d2ed4863c50c3889
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-09-09 20:26:27 +0530

    Add files via upload

---
+++ Numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+print(np.add(arr1, arr2))
+print(np.subtract(arr1, arr2))
+print(np.multiply(arr1, arr2))
+print(np.divide(arr1, arr2))
+print(np.power(arr1, arr2))
+print(np.mod(arr1, arr2))
+print(np.concatenate((arr1, arr2)))
